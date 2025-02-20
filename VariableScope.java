a363e45e87e3a10ce5f7ba20a06bbb6a5aef049e
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-02-20 08:52:49 +0800

    feat: add variable scoping in java file

---
+++ VariableScope.java
@@ -0,0 +1,13 @@
+public class VariableScope {
+    static int globalCount = 100;
+
+    public static void main(String[] args) {
+        System.out.println("Global Count (from main): " + globalCount);
+        VariableScope.showScope();
+    }
+    public static void showScope() {
+        int localCount = 50;
+        System.out.println("Global Count (from showScope): " + globalCount);
+        System.out.println("Local Count (from showScope): " + localCount);
+    }
+}
